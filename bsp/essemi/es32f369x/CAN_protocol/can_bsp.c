94c66f6eb24ef83fb5a2c5c06846a91f2def9ff1
fqzheng12 <zhengfuqiang078@hellobike.com>
2020-11-30 14:50:17 +0800

    初步实现了定时发送状态信息和接收信息处理框架

--- bsp/essemi/es32f369x/CAN_protocol/can_bsp.c
+++ bsp/essemi/es32f369x/CAN_protocol/can_bsp.c
@@ -16,23 +16,112 @@
 #define CAN_DEV_NAME       "can"
 
 
+/* 定时器的控制块 */
+static rt_timer_t timer1;//设置循环定时器进行循环发送电机速度信息
 static struct rt_semaphore rx_sem;     /* 接收信号量 */
+static struct rt_semaphore tx_sem;     /* 发送信号量 */
 static rt_device_t can_dev;  //can句柄
+struct rt_can_msg rxmsg = {0};
+struct rt_can_msg txmsg = {0};//发送帧
+rt_can_ExtendID ExtendID = {0};
+rt_can_var can_var={0};
 
+
+
+static void timeout1(void *parameter)
+{
+	
+	
+	 rt_sem_release(&tx_sem);
+
+//    return RT_EOK;
+//	  rt_size_t  size;
+////    rt_kprintf("periodic timer is timeout %d\n", cnt);
+///*111000(56，状态信息)|0（预留）|11（单帧）|111111（功能码）|000001（目标节点地址）|000010|（源节点地址）=0x70ff042*/
+//    txmsg.id = 0x70FF042;              /* ID ? 0x78 */
+////    msg.ide = RT_CAN_EXTID;     /* 扩展帧 */
+//		txmsg.ide = 1;     /* 扩展帧 */
+//    txmsg.rtr = RT_CAN_DTR;       /* 数据帧 */
+//    txmsg.len = 8;                /* ????? 8 */
+//    /* ???? 8 ???? */
+////    txmsg.data[0] = ;
+//    can_var.Drive_V5v=0x99;
+//    can_var.Drive_Vbus=0xaa;
+//    can_var.Cmd_Vq=0xbb;
+//    can_var.Drive_Ibus=0xcc;
+//    can_var.Drive_temp=0xdd;
+//    can_var.Motor_speed=0xeeff;
+//	
+//    txmsg.data[1] = can_var.Drive_V5v;
+//    txmsg.data[2] = can_var.Drive_Vbus;
+//    txmsg.data[3] = can_var.Cmd_Vq;
+//    txmsg.data[4] = can_var.Drive_Ibus;
+//    txmsg.data[5] = can_var.Drive_temp;
+//    txmsg.data[6] = (can_var.Motor_speed>>8);
+//    txmsg.data[7] = can_var.Motor_speed;
+//    /* ???? CAN ?? */
+//    size = rt_device_write(can_dev, 0, &txmsg, sizeof(txmsg));
+//    if (size == 0)
+//    {
+//        rt_kprintf("can dev write data failed!\n");
+//    }
+}
 
 static rt_err_t can_rx_call(rt_device_t dev, rt_size_t size)
 {
-    /* 接收回调含函数*///在里面释放信号量
+    /* 接收回调函数*///在里面释放信号量
     rt_sem_release(&rx_sem);
 
     return RT_EOK;
 }
 
+
+static void can_tx_thread(void *parameter)//can接收入口函数
+{
+  while(1)
+	{
+	rxmsg.hdr = -1;
+        /* 等待接收完成后回调函数释放的信号量 */
+  rt_sem_take(&tx_sem, RT_WAITING_FOREVER);//获取一次信号量，信号量减一
+	rt_size_t  size;
+//    rt_kprintf("periodic timer is timeout %d\n", cnt);
+/*111000(56，状态信息)|0（预留）|11（单帧）|111111（功能码）|000001（目标节点地址）|000010|（源节点地址）=0x70ff042*/
+    txmsg.id = 0x70FF042;              /* ID ? 0x78 */
+//    msg.ide = RT_CAN_EXTID;     /* 扩展帧 */
+		txmsg.ide = 1;     /* 扩展帧 */
+    txmsg.rtr = RT_CAN_DTR;       /* 数据帧 */
+    txmsg.len = 8;                /* ????? 8 */
+    /* ???? 8 ???? */
+//    txmsg.data[0] = ;
+    can_var.Drive_V5v=0x99;
+    can_var.Drive_Vbus=0xaa;
+    can_var.Cmd_Vq=0xbb;
+    can_var.Drive_Ibus=0xcc;
+    can_var.Drive_temp=0xdd;
+    can_var.Motor_speed=0xeeff;
+	
+    txmsg.data[1] = can_var.Drive_V5v;
+    txmsg.data[2] = can_var.Drive_Vbus;
+    txmsg.data[3] = can_var.Cmd_Vq;
+    txmsg.data[4] = can_var.Drive_Ibus;
+    txmsg.data[5] = can_var.Drive_temp;
+    txmsg.data[6] = (can_var.Motor_speed>>8);
+    txmsg.data[7] = can_var.Motor_speed;
+    /* ???? CAN ?? */
+    size = rt_device_write(can_dev, 0, &txmsg, sizeof(txmsg));
+    if (size == 0)
+    {
+        rt_kprintf("can dev write data failed!\n");
+    }
+	}	
+
+}
 static void can_rx_thread(void *parameter)//can接收入口函数
 {
-    int i;
+//    int i;
     rt_err_t res;
-    struct rt_can_msg rxmsg = {0};
+    
+		
 
     /* 设置回调函数 */
     rt_device_set_rx_indicate(can_dev, can_rx_call);
@@ -60,23 +149,74 @@ static void can_rx_thread(void *parameter)//can接收入口函数
         rt_sem_take(&rx_sem, RT_WAITING_FOREVER);//获取一次信号量，信号量减一
         /* ? CAN 有信号量了之后，读取一帧数据 */
         rt_device_read(can_dev, 0, &rxmsg, sizeof(rxmsg));
-        /* 将接收到的can ID打印出来*/
-        rt_kprintf("ID:%x\n", rxmsg.id);
-        for (i = 0; i < 8; i++)
-        {
-            rt_kprintf("%2x ", rxmsg.data[i]);
-        }
+			     if(rxmsg.ide==1)//如果是远程帧的话就把帧ID按协议分解
+					 {
+					 ExtendID.can_cmd=((rxmsg.id>>21)&0x7f);//右移22位留下高7位。
+					 ExtendID.can_kbit=((rxmsg.id>>20)&0x01);//右移21位留下高1位。
+					 ExtendID.can_fflag=((rxmsg.id>>18)&0x7);//右移18位留下高3位。
+					 ExtendID.can_func=((rxmsg.id>>12)&0x3f);//右移12位留下高6位。 
+				   ExtendID.can_dest=((rxmsg.id>>6)&0x3f);//右移6位留下高6位。
+				   ExtendID.can_addr=(rxmsg.id&0x3f);//留下高6位。
 
-        rt_kprintf("\n");
+					  
+					 
+			
+        /* 将接收到的can ID打印出来*/
+           rt_kprintf("ID:%x\n", rxmsg.id);
+					 rt_kprintf("\n");
+//        for (i = 0; i < 8; i++)
+//        {
+           rt_kprintf("%2x ", ExtendID.can_cmd);//打出来验证下分解的有没有问题
+				   rt_kprintf("%2x ", ExtendID.can_kbit);
+				   rt_kprintf("%2x ", ExtendID.can_fflag);
+				   rt_kprintf("%2x ", ExtendID.can_func);
+				   rt_kprintf("%2x ", ExtendID.can_dest);
+				   rt_kprintf("%2x ", ExtendID.can_addr);
+//        }
+
+           rt_kprintf("\n");
+					 switch(ExtendID.can_cmd)
+					   {
+						 case(11):
+						 can_emergency();
+						 break;
+						 
+						 case(26):
+						 can_exception();
+						 break;
+						 
+						 case(41):
+						 can_control_cmd();
+						 break;
+						 
+						 case(56):
+						 can_drive_staus();
+						 break;
+						 
+						 case(71):
+						 can_device_message();
+						 break;
+						 
+						 case(86):
+						 can_up_download();
+						 break;
+						 
+						 case(101):
+						 can_upgrade();
+						 break;
+					   }
+					 }
+						
     }
 }
 
 int can_sample(int argc, char *argv[])
 {
-    struct rt_can_msg msg ;
+//    struct rt_can_msg msg ;
     rt_err_t res;
-    rt_size_t  size;
+//    rt_size_t  size;
     rt_thread_t can_rx;
+	  rt_thread_t can_tx;
     char can_name[RT_NAME_MAX];
 
     if (argc == 2)
@@ -97,6 +237,9 @@ int can_sample(int argc, char *argv[])
 
     /* 初始化can接收信号量 */
     rt_sem_init(&rx_sem, "rx_sem", 0, RT_IPC_FLAG_FIFO);
+		/* 初始化can发送信号量 */
+    rt_sem_init(&tx_sem, "tx_sem", 0, RT_IPC_FLAG_FIFO);
+		
 
     /* ???????????? CAN ?? */
     res = rt_device_open(can_dev, RT_DEVICE_FLAG_INT_TX | RT_DEVICE_FLAG_INT_RX);
@@ -112,30 +255,122 @@ int can_sample(int argc, char *argv[])
     {
         rt_kprintf("create can_rx thread failed!\n");
     }
-
-    msg.id = 0x7FF;              /* ID ? 0x78 */
-//    msg.ide = RT_CAN_EXTID;     /* 扩展帧 */
-		msg.ide = 0;     /* 扩展帧 */
-    msg.rtr = RT_CAN_DTR;       /* 数据帧 */
-    msg.len = 8;                /* ????? 8 */
-    /* ???? 8 ???? */
-    msg.data[0] = 0x00;
-    msg.data[1] = 0x22;
-    msg.data[2] = 0x22;
-    msg.data[3] = 0x33;
-    msg.data[4] = 0x44;
-    msg.data[5] = 0x55;
-    msg.data[6] = 0x66;
-    msg.data[7] = 0x77;
-    /* ???? CAN ?? */
-    size = rt_device_write(can_dev, 0, &msg, sizeof(msg));
-    if (size == 0)
+/*can发送线程*/	
+		can_tx = rt_thread_create("can_tx", can_tx_thread, RT_NULL, 1024, 25, 10);//动态线程
+    if (can_tx != RT_NULL)
     {
-        rt_kprintf("can dev write data failed!\n");
+        rt_thread_startup(can_tx);
     }
+    else
+    {
+        rt_kprintf("create can_tx thread failed!\n");
+    }
+		
+/*设置循环定时器进行驱动板状态信息发送*/
+timer1 = rt_timer_create("timer1", timeout1,
+                             RT_NULL, 3000,//3秒钟释放一个信号量，发送数据。
+                             RT_TIMER_FLAG_PERIODIC);		
+    /* 启动定时器1 */
+    if (timer1 != RT_NULL) rt_timer_start(timer1);		
+		
+//    msg.id = 0x7FF;              /* ID ? 0x78 */
+////    msg.ide = RT_CAN_EXTID;     /* 扩展帧 */
+//		msg.ide = 0;     /* 扩展帧 */
+//    msg.rtr = RT_CAN_DTR;       /* 数据帧 */
+//    msg.len = 8;                /* ????? 8 */
+//    /* ???? 8 ???? */
+//    msg.data[0] = 0x00;
+//    msg.data[1] = 0x22;
+//    msg.data[2] = 0x22;
+//    msg.data[3] = 0x33;
+//    msg.data[4] = 0x44;
+//    msg.data[5] = 0x55;
+//    msg.data[6] = 0x66;
+//    msg.data[7] = 0x77;
+//    /* ???? CAN ?? */
+//    size = rt_device_write(can_dev, 0, &msg, sizeof(msg));
+//    if (size == 0)
+//    {
+//        rt_kprintf("can dev write data failed!\n");
+//    }
 
     return res;
 }
 MSH_CMD_EXPORT(can_sample, can device sample);
 
+//int timer1_sample(void)
+//{
+//    /* 创建定时器1  周期定时器 */
+//    timer1 = rt_timer_create("timer1", timeout1,
+//                             RT_NULL, 10,
+//                             RT_TIMER_FLAG_PERIODIC);
+
+//    /* 启动定时器1 */
+//    if (timer1 != RT_NULL) rt_timer_start(timer1);
+
+////    /* 创建定时器2 单次定时器 */
+////    timer2 = rt_timer_create("timer2", timeout2,
+////                             RT_NULL,  30,
+////                             RT_TIMER_FLAG_ONE_SHOT);
+
+////    /* 启动定时器2 */
+////    if (timer2 != RT_NULL) rt_timer_start(timer2);
+//    return 0;
+//}
+
+/* 导出到 msh 命令列表中 */
+//MSH_CMD_EXPORT(timer1_sample, timer sample);
+
+/*报警信息处理*/
+void can_emergency(void)
+{
+//
+}
+
+/*紧急信息处理*/
+void can_exception(void)
+{
+//
+}
+
+
+
+
+/*控制信息处理*/
+void can_control_cmd(void)
+{
+	switch(ExtendID.can_func)
+		case(0x3f)://如果是电机实时控制寄存器，进行寄存器值更新
+			can_var.Cmd_Iq=((rxmsg.data[6]<<8)&rxmsg.data[7]);
+	    can_var.Drive_en=rxmsg.data[5];
+	    can_var.Drive_off=(rxmsg.data[5]>>1);
+	    can_var.Mweek_en=(rxmsg.data[5]>>2);
+		  can_var.MTPA_en=(rxmsg.data[5]>>3);
+		  can_var.Security_model_en=(rxmsg.data[5]>>4);
+	    can_var.Mweek_deep=rxmsg.data[4];
+	    can_var.MTPA_deep=rxmsg.data[3];
+	    can_var.Security_model_current=rxmsg.data[2];
+//	  break;
+}
+
+/*驱动状态信息处理*/
+void can_drive_staus(void)
+{
+//
+}
+/*设备信息处理*/
+void can_device_message(void)
+{
+//
+}
+/*上传下载信息处理*/
+void can_up_download(void)
+{
+//
+}
+/*升级信息处理*/
+void can_upgrade(void)
+{
+//
+}
 
--- bsp/essemi/es32f369x/CAN_protocol/can_bsp.h
+++ bsp/essemi/es32f369x/CAN_protocol/can_bsp.h
@@ -6,7 +6,55 @@
 #ifndef __CAN_BSP_H__
 #define __CAN_BSP_H__
 
+#include <rtthread.h>
 
+
+void can_emergency(void);//紧急信息处理
+void can_exception(void);//报警信息处理
+void can_control_cmd(void);//控制命令处理
+void can_drive_staus(void);//驱动状态信息处理
+void can_device_message(void);//设备信息处理
+void can_up_download(void);//上传下载数据函数
+void can_upgrade(void);//升级相关命令函数
+
+
+//int timer1_sample(void);
+
+typedef struct 
+{
+    rt_uint8_t can_cmd  : 7;//优先级
+    rt_uint8_t can_kbit : 1;//保留位
+    rt_uint8_t can_fflag : 3;//can帧标志
+    rt_uint8_t can_func : 6;//编号子功能
+    rt_uint8_t can_dest : 6;//目标节点
+    rt_uint8_t can_addr : 6;//源节点地址
+   
+}rt_can_ExtendID;
+
+
+typedef struct 
+{
+  
+/*控制信息   */
+  rt_uint16_t Cmd_Iq;
+	rt_uint8_t Drive_en:	1;
+	rt_uint8_t Drive_off:	1;
+	rt_uint8_t Mweek_en:	1;
+	rt_uint8_t Mweek_deep:	3;
+	rt_uint8_t MTPA_en:	1;
+	rt_uint8_t MTPA_deep:	4;
+	rt_uint8_t Security_model_en:1;
+	rt_uint8_t Security_model_current:6;
+	
+/*状态信息   */ 
+	rt_uint16_t Motor_speed;
+	rt_uint8_t Drive_temp;
+	rt_uint8_t Drive_Ibus;
+	rt_uint8_t Cmd_Vq;
+	rt_uint8_t Drive_Vbus;
+	rt_uint8_t Drive_V5v;
+
+}rt_can_var;
 
 
 
